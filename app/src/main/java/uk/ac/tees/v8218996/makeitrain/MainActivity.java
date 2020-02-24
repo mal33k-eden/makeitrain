a123ba9802ef62228498fa76a630de012553679e
edenated@gmail.com <gazelleinc.1z>
2020-02-24 18:27:10 +0000

    adding toast update

--- app/src/main/java/uk/ac/tees/v8218996/makeitrain/MainActivity.java
+++ app/src/main/java/uk/ac/tees/v8218996/makeitrain/MainActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -53,29 +54,38 @@ public class MainActivity extends AppCompatActivity {
             case 5000: //black money
 
                  bk = getResources().getColor(R.color.colorBlack);
-                updateView(Color.WHITE,bk);
+                 updateView(Color.WHITE,bk);
+
+                updateToast("Making it rain with BLACK money");
 
             break;
             case 10000: //yellow money
                  bk = getResources().getColor(R.color.colorYellow);
                 updateView(Color.WHITE,bk);
 
+                updateToast("You have gotten yourself some YELLOW money");
+
                 break;
 
             case 15000: //pink money
                 bk = getResources().getColor(R.color.colorAccent);
                 updateView(Color.WHITE,bk);
+                updateToast("Pinkish Money is stylish money");
 
                 break;
             case 18000: //red money
                 bk = getResources().getColor(R.color.colorRed);
                 updateView(Color.WHITE,bk);
 
+                updateToast("HA! Your EYE RED  for RED MONEY!!!");
+
                 break;
             case 20000: //green money
 
                 bk = getResources().getColor(R.color.colorPrimaryDark);
                 updateView(Color.WHITE,bk);
+
+                updateToast("IT ALL GREEN AT THE HIGHER SIDE.... GREEN money");
 
                 break;
 
@@ -85,13 +95,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateView(int textColor,int backColor){
 
-
-
-
-
         title.setTextColor(textColor);
         updateAmount.setTextColor(textColor);
         mainView.setBackgroundColor(backColor);
+
+    }
+
+    public void updateToast(String msg){
+
+        Toast.makeText(MainActivity.this,msg, Toast.LENGTH_LONG)
+                .show();
 
     }
 
