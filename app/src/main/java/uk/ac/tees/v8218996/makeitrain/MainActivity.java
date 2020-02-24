67e781ee0d87cd780440389d4489cf5486426b02
edenated@gmail.com <gazelleinc.1z>
2020-02-24 18:18:40 +0000

    adding color update and background update

--- app/src/main/java/uk/ac/tees/v8218996/makeitrain/MainActivity.java
+++ app/src/main/java/uk/ac/tees/v8218996/makeitrain/MainActivity.java
@@ -1,28 +1,39 @@
 package uk.ac.tees.v8218996.makeitrain;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.util.Log;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView title;
     private TextView updateAmount;
     private int amount = 1000;
     private NumberFormat cur = NumberFormat.getCurrencyInstance();
+    private View mainView;
+    private int bk;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        title = findViewById(R.id.textView);
         updateAmount = findViewById(R.id.textView2);
 
-        updateAmount.setText(String.valueOf(cur.format(amount)));
+        mainView =  findViewById(R.id.main_view);
+
+        updateAmount.setText(cur.format(amount));
 
 
 
@@ -35,8 +46,55 @@ public class MainActivity extends AppCompatActivity {
         //NumberFormat format = NumberFormat.getCurrencyInstance();
 
 
-        updateAmount.setText(String.valueOf(cur.format(amount)));
+        updateAmount.setText(cur.format(amount));
 
-        //Log.d("checks", "amount is " + amount);
+        switch(amount){
+
+            case 5000: //black money
+
+                 bk = getResources().getColor(R.color.colorBlack);
+                updateView(Color.WHITE,bk);
+
+            break;
+            case 10000: //yellow money
+                 bk = getResources().getColor(R.color.colorYellow);
+                updateView(Color.WHITE,bk);
+
+                break;
+
+            case 15000: //pink money
+                bk = getResources().getColor(R.color.colorAccent);
+                updateView(Color.WHITE,bk);
+
+                break;
+            case 18000: //red money
+                bk = getResources().getColor(R.color.colorRed);
+                updateView(Color.WHITE,bk);
+
+                break;
+            case 20000: //green money
+
+                bk = getResources().getColor(R.color.colorPrimaryDark);
+                updateView(Color.WHITE,bk);
+
+                break;
+
+        }
+
     }
+
+    public void updateView(int textColor,int backColor){
+
+
+
+
+
+        title.setTextColor(textColor);
+        updateAmount.setTextColor(textColor);
+        mainView.setBackgroundColor(backColor);
+
+    }
+
+
+
 }
